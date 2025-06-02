9c6d9593f84fe880f7881623e76a122a992ddec9
unknown <charmainetharaka@gmail.com>
2025-06-02 13:21:53 +0200

    Function update

--- Classroom_survey.py
+++ Classroom_survey.py
@@ -1,21 +1,36 @@
-Question = ["Are lists part of python data structure?", 
-            "Are for loops and while loops the same? ",
-            "is .append, .join, .remove part of built-in function" 
-            ]
 
+questions = [
+    "Are lists part of Python data structure? ",
+    "Are for loops and while loops the same? ",
+    "Is .append, .join, .remove part of built-in function? "
+]
 
-def Question_to_ask():
-    response = []
-    for item in Question:
-        #The range asks how many questions are there and the loop will work with the number
-        print("\n" * 2)
-        # print("These are Yes or No Question!!!!!")
-        answer = input(item + " "+ "")
-        response.append(answer)
-        print(answer)
-
-    print(response)
-
-# to call the function 
-Question_to_ask()
+def ask_questions():
+    """Asks questions and collects responses"""
+    responses = []
+    for question in questions:
+        print("\n")
+        answer = input(question).strip().lower()
+        responses.append(answer)
+        print(f"You answered: {answer}")
     
+    print("\nYour responses:")
+    print(responses)
+    return responses
+
+def count_responses(responses):
+    """Counts the occurrences of each response"""
+    answers = {}
+    for response in responses:
+        if response in answers:
+            answers[response] += 1
+        else:
+            answers[response] = 1 
+    return answers
+
+# Ask questions and collect responses
+responses = ask_questions()
+
+# Count and print response occurrences
+print("\nResponse counts:")
+print(count_responses(responses))
