b58d2b72692e4771d60506d4c7c70830d91d910d
Process <nyalunguprocess@gmail.com>
2025-06-02 11:01:20 +0200

    Fuction and questions

--- Classroom_survey.py
+++ Classroom_survey.py
@@ -1,1 +1,21 @@
-Question = [["Are lists part of python data structure? "], ["Are for loops and while loops the same? "], ["is .append, .join, .remove part of built-in function? "]]
+Question = ["Are lists part of python data structure?", 
+            "Are for loops and while loops the same? ",
+            "is .append, .join, .remove part of built-in function" 
+            ]
+
+
+def Question_to_ask():
+    response = []
+    for item in Question:
+        #The range asks how many questions are there and the loop will work with the number
+        print("\n" * 2)
+        # print("These are Yes or No Question!!!!!")
+        answer = input(item + " "+ "")
+        response.append(answer)
+        print(answer)
+
+    print(response)
+
+# to call the function 
+Question_to_ask()
+    
