61b94a1fe5ca2e6dbfca56ab556f198e93933ca6
Oletilwe <Oletilwelephalala8@gmail.com>
2025-06-02 13:32:17 +0200

    Added comments and updated some of the code

--- Classroom_survey.py
+++ Classroom_survey.py
@@ -1,21 +1,39 @@
-Question = ["Are lists part of python data structure?", 
-            "Are for loops and while loops the same? ",
-            "is .append, .join, .remove part of built-in function" 
-            ]
+# this is a list of question we are going to use. 
+questions = [
+    "Are lists part of Python data structure? ",
+    "Are for loops and while loops the same? ",
+    "Is .append, .join, .remove part of built-in function? "
+]
+# function to ask the user to ask the user their input 
+def ask_questions():
+    """Asks questions and collects responses"""
+    # empty list 
+    responses = []
+    for question in questions:
+        print("\n")
+        # .lower is used to take the input of the user and turn it to lowercase
+        answer = input(question).strip().lower()
+        responses.append(answer)
+        print(f"You answered: {answer}")
+    #the response will be on a new line
+    print("\nYour responses:")
+    print(responses)
+    return responses
 
+def count_responses(responses):
+    """Counts the occurrences of each response"""
+#empty dictionary 
+    answers = {}
+    for response in responses:
+        if response in answers:
+            answers[response] += 1
+        else:
+            answers[response] = 1 
+    return answers
 
-def Question_to_ask():
-    response = []
-    for item in Question:
-        #The range asks how many questions are there and the loop will work with the number
-        print("\n" * 2)
-        # print("These are Yes or No Question!!!!!")
-        answer = input(item + " "+ "")
-        response.append(answer)
-        print(answer)
+# Ask questions and collect responses
+responses = ask_questions()
 
-    print(response)
-
-# to call the function 
-Question_to_ask()
-    
+# Count and print response occurrences
+print("\nResponse counts:")
+print(count_responses(responses))
