afbddb95a18db372221a2171084b943933c60ad2
Process <nyalunguprocess@gmail.com>
2025-06-03 13:43:44 +0200

    New libraty class and methods

--- Community_library_system.py
+++ Community_library_system.py
@@ -13,6 +13,29 @@ class Book:
             return f"'{self.title}' has been lent to {out_to_user}."
         else:
             return f"'{self.title}' is currently not available."
-        
+
+My_book = Book("surrounded by idiots", "Le Hao", "1254-95478")
+
+hello = My_book.lend("Oletilwe")
+print(hello)
 
 
+class Library:
+     def __init__(self, author, title, book_list):
+         self.author = author
+         self.title = title
+         self.book_list = book_list
+         
+book_list = {
+        "nothing but the truth" : "Pro",
+        "Le gae" : " charmaine",
+        "48 Laws of power" : "Tsitso",
+        "Think like a man" : "ntsako",
+        "The psychology of money" : "titi",
+     }
+         
+    # def find_book_by_author(self, keyword):
+    #     for book in book_list:
+    #         if book in book_list:
+    #             keyword.lower 
+    #         return book
